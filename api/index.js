777826b960cf1545e19099d7ac3235198e8e6096
Paul Karabilo <benizebe@gmail.com>
2017-08-01 16:28:52 +0200

    aded simple cache

--- api/index.js
+++ api/index.js
@@ -8,30 +8,37 @@ const client = redis.createClient({host: 'db', port: 6379});
 const SYM = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'.split('');
 const BASE = SYM.length;
 
+let cache = {};
+
 let convert = (n) => n ? SYM[n % BASE] + convert(Math.floor(n / BASE)) : '';
 //let unconvert = (s, p = 0) => s && s.length ? unconvert(s.substr(1), p + 1) + SYM.indexOf(s[0]) * Math.pow(BASE, p) : 0
 
 app.get('/', (req, res) => res.send('hello world'));
 
 app.get('/api/min/:url', (req, res) => {
-    client.incr('current_url_id', function (err, id) {
+    let url = decodeURI(req.params.url);
+    if (cache[url]) {
+        return res.status(200).send(cache[url]);
+    }
+    client.incr('current_url_id', (err, id) => {
         let cid = convert(id);
         if (err) {
             console.error(err);
             return res.status(500).send('ERROR: Could not increment ID');
         }
-        client.set(`urls:${cid}`, decodeURI(req.params.url), function (err) {
+        client.set(`urls:${cid}`, url, (err) => {
             if (err) {
                 console.error(err);
                 return res.status(500).send('ERROR: Could not write URL to DB');
             }
+            cache[url] = cid;
             res.status(200).send(cid);
         });
     });
 });
 
 app.get('/api/demin/:cid', (req, res) => {
-    client.get(`urls:${req.params.cid}`, function (err, url) {
+    client.get(`urls:${req.params.cid}`, (err, url) => {
         if (err) {
             console.error(err);
             return res.status(500).send('ERROR: Could not fetch URL from DB');
