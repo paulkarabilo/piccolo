10caa5a1153f39ef9f44c551db45840a039dd0ba
Paul Karabilo <benizebe@gmail.com>
2017-08-01 13:35:43 +0200

    simple db impl

--- api/index.js
+++ api/index.js
@@ -2,24 +2,42 @@
 
 const express = require('express');
 const app = express();
-let urls = [];
+const redis = require('redis');
+const client = redis.createClient({host: 'db', port: 6379});
+
+const SYM = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'.split('');
+const BASE = SYM.length;
+
+let convert = (n) => n ? SYM[n % BASE] + convert(Math.floor(n / BASE)) : '';
+//let unconvert = (s, p = 0) => s && s.length ? unconvert(s.substr(1), p + 1) + SYM.indexOf(s[0]) * Math.pow(BASE, p) : 0
 
 app.get('/', (req, res) => res.send('hello world'));
+
 app.get('/api/min/:url', (req, res) => {
-    let url = decodeURI(req.params.url);
-    let i = urls.indexOf(url);
-    if (i === -1) {
-        i = urls.push(url) - 1;
-    }
-    res.send(new Buffer(`${i}`).toString('base64'));
+    client.incr('current_url_id', function (err, id) {
+        let cid = convert(id);
+        if (err) {
+            console.error(err);
+            return res.status(500).send('ERROR: Could not increment ID');
+        }
+        client.set(`urls:${cid}`, decodeURI(req.params.url), function (err) {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('ERROR: Could not write URL to DB');
+            }
+            res.status(200).send(cid);
+        });
+    });
 });
-app.get('/api/demin/:b64', (req, res) => {
-    let b64 = req.params.b64;
-    let i = Number(new Buffer(b64, 'base64').toString('ascii'));
-    if (i && urls[i]) {
-        res.send(urls[i]);
-    } else {
-        res.send(-1);
-    }
+
+app.get('/api/demin/:cid', (req, res) => {
+    client.get(`urls:${req.params.cid}`, function (err, url) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('ERROR: Could not fetch URL from DB');
+        }
+        res.status(200).send(url);
+    });
 });
+
 app.listen(3000, '0.0.0.0', () => console.log("api running at http://localhost:3000/api"));
